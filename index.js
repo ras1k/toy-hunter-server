015daee43dac2ffc2c2d5dbd22ff34f7f0cf44b9
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-15 13:45:44 +0600

    toys added

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const toyCollection = client.db('toyDB').collection('toys');
+
+    //services route
+    app.get('/toys', async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
